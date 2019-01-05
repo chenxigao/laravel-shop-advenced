d3de20326a642b167e2dbcc16c1d00c220547373
vagrant <vagrant@homestead>
2019-01-05 08:26:51 +0000

    顶部导航加入商品类目树

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Exceptions\InvalidRequestException;
+use App\Services\CategoryService;
 use function foo\func;
 use Illuminate\Http\Request;
 use App\Models\Product;
@@ -11,7 +12,8 @@ use App\Models\Category;
 
 class ProductsController extends Controller
 {
-    public function index(Request $request)
+    // 使用 Laravel 的依赖注入，自动创建 $categoryService 对象
+    public function index(Request $request, CategoryService $categoryService)
     {
         // 创建一个查询构造器
         $builder = Product::query()->where('on_sale', true);
@@ -68,6 +70,8 @@ class ProductsController extends Controller
             ],
             //等价于 isset($category) ? $category : null
             'category' => $category ?? null,
+            // 将类目树传递给模板文件
+            'categoryTree' => $categoryService->getCategoryTree(),
         ]);
     }
 
--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -13,7 +13,19 @@
         </div>
         <div class="collapse navbar-collapse" id="app-navbar-collapse">
             <ul class="nav navbar-nav">
+                <!-- 顶部类目菜单开始 -->
+                <!-- 判断模板是否有 $categoryTree 变量 -->
+                @if(isset($categoryTree))
+                    <li>
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">所有类目 <b class="caret"></b></a>
+                        <ul class="dropdown-menu multi-level">
+                            <!-- 遍历 $categoryTree 集合，将集合中的每一项 $category 变量注入 layouts._category_item 模板中并渲染-->
+                            @each('layouts._category_item', $categoryTree, 'category')
+                        </ul>
+                    </li>
+                @endif
             </ul>
+            <!-- 顶部类目菜单栏结束 -->
             <ul class="nav navbar-nav navbar-right">
                 <!-- 登录注册链接开始 -->
                 @guest
